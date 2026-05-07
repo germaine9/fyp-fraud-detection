cd7c7e374602b639443fc58737e6d4eeef278bf3
germaine9 <germaineying@gmail.com>
2026-05-07 08:47:37 +0800

    Add model comparison and confusion matrix visualization

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix
+)
+
+from imblearn.over_sampling import SMOTE
+
 # 1. Load dataset
 df = pd.read_csv("healthcare_fraud_detection.csv")
 
@@ -9,7 +23,7 @@ df = pd.read_csv("healthcare_fraud_detection.csv")
 df = df.drop(['Provider_ID', 'Claim_ID'], axis=1)
 
 # 3. Handle missing values
-df = df.fillna(method='ffill')
+df = df.ffill()
 
 # 4. Convert text to numbers
 df = pd.get_dummies(df)
@@ -19,37 +33,42 @@ X = df.drop('Is_Fraud', axis=1)
 y = df['Is_Fraud']
 
 # 6. Train-test split
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-from imblearn.over_sampling import SMOTE
-
+# 7. Handle imbalance using SMOTE
 sm = SMOTE(random_state=42)
 X_train, y_train = sm.fit_resample(X_train, y_train)
 
-# 7. Train model
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
+# 8. Models to compare
+models = {
+    "Random Forest": RandomForestClassifier(),
+    "Logistic Regression": LogisticRegression(max_iter=1000),
+    "Decision Tree": DecisionTreeClassifier()
+}
 
-# 8. Predict
-pred = model.predict(X_test)
+# 9. Train and evaluate models
+for name, model in models.items():
 
-# 9. Evaluate
-from sklearn.metrics import classification_report
-print(classification_report(y_test, pred))
+    model.fit(X_train, y_train)
 
-from sklearn.metrics import confusion_matrix
+    pred = model.predict(X_test)
 
-cm = confusion_matrix(y_test, pred)
+    print(f"\n{name}")
+    print(classification_report(y_test, pred))
 
-plt.figure(figsize=(6,4))
-sns.heatmap(cm, annot=True, fmt='d')
+    acc = accuracy_score(y_test, pred)
+    print("Accuracy:", acc)
 
-plt.xlabel("Predicted")
-plt.ylabel("Actual")
-plt.title("Confusion Matrix")
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, pred)
 
-plt.show()
+    plt.figure(figsize=(6,4))
+    sns.heatmap(cm, annot=True, fmt='d')
+
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title(f"Confusion Matrix - {name}")
+
+    plt.show()
